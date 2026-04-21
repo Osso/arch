f8aae09fc09537fe1741a74e1ddb02c42c476cd3
Alessio Deiana <adeiana@gmail.com>
2026-04-21 15:33:42 -0500

    reduce search run cognitive complexity

--- src/commands/search.rs
+++ src/commands/search.rs
@@ -1,38 +1,51 @@
 use crate::alpm_handle;
 use anyhow::Result;
 
+fn package_matches_pattern(pkg: &alpm::Package, pattern_lower: &str, search_desc: bool) -> bool {
+    if pkg.name().to_lowercase().contains(pattern_lower) {
+        return true;
+    }
+
+    if !search_desc {
+        return false;
+    }
+
+    pkg.desc()
+        .map(|description| description.to_lowercase().contains(pattern_lower))
+        .unwrap_or(false)
+}
+
+fn print_search_result(pkg: &alpm::Package, db_name: &str, installed: bool) {
+    let installed_marker = if installed { " [installed]" } else { "" };
+    println!(
+        "{}/{} {}{}",
+        db_name,
+        pkg.name(),
+        pkg.version(),
+        installed_marker
+    );
+
+    if let Some(desc) = pkg.desc() {
+        println!("    {}", desc);
+    }
+}
+
 /// Search for packages in sync databases
 pub fn run(pattern: &str, search_desc: bool) -> Result<()> {
     let handle = alpm_handle::init_readonly()?;
     let pat_lower = pattern.to_lowercase();
+    let local_db = handle.localdb();
     let mut found_any = false;
 
     for db in handle.syncdbs() {
         for pkg in db.pkgs() {
-            let name_match = pkg.name().to_lowercase().contains(&pat_lower);
-            let desc_match = search_desc
-                && pkg
-                    .desc()
-                    .map(|d| d.to_lowercase().contains(&pat_lower))
-                    .unwrap_or(false);
-
-            if name_match || desc_match {
-                found_any = true;
-                let installed = handle.localdb().pkg(pkg.name()).is_ok();
-                let installed_marker = if installed { " [installed]" } else { "" };
-
-                println!(
-                    "{}/{} {}{}",
-                    db.name(),
-                    pkg.name(),
-                    pkg.version(),
-                    installed_marker
-                );
-
-                if let Some(desc) = pkg.desc() {
-                    println!("    {}", desc);
-                }
+            if !package_matches_pattern(pkg, &pat_lower, search_desc) {
+                continue;
             }
+
+            found_any = true;
+            let installed = local_db.pkg(pkg.name()).is_ok();
+            print_search_result(pkg, db.name(), installed);
         }
     }
 
